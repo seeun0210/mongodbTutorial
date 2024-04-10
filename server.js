995ea767f071a2dba70c6d01dbf0af1302ddfdbf
kimseeun <cocobell3@naver.com>
2024-04-10 21:04:13 +0900

    feat: 서버 기본 세팅

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("hello world!!!");
+});
+
+app.listen(80, () => {
+  console.log("server listening port:80");
+});
